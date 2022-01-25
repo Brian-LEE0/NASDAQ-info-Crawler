f175559ed335b8dbe6d91d3a9fd37fa70587dad8
NNAisle <liy908@naver.com>
2022-01-26 06:30:18 +0900

    Update nasdaq.py

--- 0.2.0/nasdaq.py
+++ 0.2.0/nasdaq.py
@@ -231,7 +231,6 @@ def judgeval(tickerfull, ticker, key, variance, inc_emoji, dec_emoji, tothemoon_
 		global shortsqueezelock
 		if current_time.hour >= 4 and current_time.hour < 21 and current_time.weekday() != 5 and current_time.weekday() != 6:
 			price_info = stock_info_upd(ticker)
-			print("a")
 			##open notice
 			if current_time.hour == OPEN_TIME[0] and current_time.minute >= OPEN_TIME[1] and price_info[4] == 0 and market_open_token[key] == 1 :
 				mes[key] = f'[장 시작]\n{ticker} 주가!\n<{str(price_info[0])}$, {price_info[3]}>'
@@ -253,8 +252,6 @@ def judgeval(tickerfull, ticker, key, variance, inc_emoji, dec_emoji, tothemoon_
 					market_close_token[key] = 0
 					return 0
 				
-
-			print("b")
 
 			#duplicationError judg
 			if price_buf1[key] != price_info[0] and price_buf2[key] != price_info[0] and price_buf3 != price_info[0] :
